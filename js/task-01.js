92d460cffc8ef26bc6cc22ea1125e50802170c12
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2020-09-16 14:01:44 +0300

    додає розв'язання задачі 1-3

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 const name = "«Генератор защитного поля»"; // Write code on this line
 let price = 1000; // Write code on this line
-const firstMessage = `Выбран «Генератор защитного поля», цена за штуку 1000 кредитов`; // Write code on this line
+const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
 price = 2000;
-const secondMessage = `Выбран «Генератор защитного поля», цена за штуку 2000 кредитов`; // Write code on this line
+const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
 
 console.log(firstMessage);
 // 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
